a9f373bfd6a18dce85d20d17f723ec98637b5fca
Rhaniel <rhanielmag.xavier@gmail.com>
2025-09-25 15:33:40 -0300

    refactor: changed footer links and added missing ones

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home.tsx";
 import { Professores } from "./pages/Professores.tsx";
 import { ProfessorDetail } from "./pages/ProfessorDetail.tsx";
 import { Sobre } from "./pages/Sobre.tsx";
+import { SiFacebook, SiInstagram, SiYoutube } from "react-icons/si";
 
 import logoUANE from './assets/logo-UANE.png'
 import wppLogo from './assets/wpp-logo.svg'
@@ -37,12 +38,30 @@ function App() {
             <div className='flex flex-col '>
               <span className='font-semibold text-2xl text-[#555]'>Fale Conosco</span>              
               <div>
-                <img className='inline mr-2 size-5' src={wppLogo} alt="Logo WhatsApp" />
-                <span className='text-[#138E98]'>(85) 99332-8888</span>
+                <a
+                  href="https://wa.me/5585993328888"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center hover:underline"
+                >
+                  <img className='inline mr-2 size-5' src={wppLogo} alt="Logo WhatsApp" />
+                  <span className='text-[#138E98]'>(85) 99332-8888</span>
+                </a>
               </div>
             </div>
             <div className='flex flex-col text-center lg:text-left '>
-              <span className='font-semibold text-2xl text-[#555]'>Nos acompanhe nas redes sociais</span>
+              <span className='font-semibold text-2xl text-[#555] mb-2'>Nos acompanhe nas redes sociais</span>
+              <div className="flex justify-center lg:justify-start gap-4 mt-2 mb-4">
+                <a href="https://www.facebook.com/OPOVO/" target="_blank" rel="noopener noreferrer">
+                  <SiFacebook className="w-7 h-7 text-[#1F6482] hover:text-[#1877f2] transition-colors" />
+                </a>
+                <a href="https://www.instagram.com/opovoonline/" target="_blank" rel="noopener noreferrer">
+                  <SiInstagram className="w-7 h-7 text-[#1F6482] hover:text-[#E1306C] transition-colors" />
+                </a>
+                <a href="https://www.youtube.com/c/opovo/videos" target="_blank" rel="noopener noreferrer">
+                  <SiYoutube className="w-7 h-7 text-[#1F6482] hover:text-[#FF0000] transition-colors" />
+                </a>
+              </div>
             </div>
           </div>
           
